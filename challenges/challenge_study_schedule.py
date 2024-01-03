27a0907439bad379d38d9274576df99e91e02eea
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2024-01-03 15:04:35 -0300

    req 01 - study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    count = 0
+    for student in permanence_period:
+        if all((
+                isinstance(student[0], int),
+                isinstance(student[1], int),
+                isinstance(target_time, int))):
+            if student[0] <= target_time and student[1] >= target_time:
+                count += 1
+        else:
+            return None
+    return count
